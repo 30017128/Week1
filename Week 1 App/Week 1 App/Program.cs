d61825b7c23fd8c6d486954ac283e45397932ba3
Stephen Burgess <30017128@student.toiohomai.ac.nz>
2019-02-27 10:44:56 +1300

    first commit

---
+++ Week 1 App/Week 1 App/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Week_1_App
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            // Will print Hello World to the screen
+
+            int age;
+
+            string name;
+
+            Console.WriteLine("Please enter your age.");
+            //Parse is checking if it is a integer 
+            age = int.Parse (Console.ReadLine());
+            Console.WriteLine("Please enter your name");
+            name = Console.ReadLine();
+            Console.WriteLine(name + age);
+
+
+            Console.ReadLine();
+           
+        }
+    }
+}
